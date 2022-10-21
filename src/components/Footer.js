7a8e54e0da486e075963c9146f513d42447c2fdf
luisv7 <luisvillarreal_7@icloud.com>
2022-10-21 15:46:06 -0700

    new footer format and link to linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
+import { Typography, Container, Link } from "@mui/material";
 
 const Footer = () => {
     return (
-        <footer>    
-                <h4>&copy;2022 Luis Villarreal</h4>
-        </footer>
+        <Container maxWidth={false} component="footer">
+                <Typography variant="subtitle1" component="h5" align="center">
+                    &copy;2022&nbsp;
+                    <Link href="https://www.linkedin.com/in/luis-villarreal7/" underline="hover" target="_blank" sx={{color: "#000"}}>
+                        Luis Villarreal
+                    </Link>
+                </Typography>
+        </Container>
     )
 }
 export default Footer;
