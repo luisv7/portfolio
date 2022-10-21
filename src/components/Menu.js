852930ba56f4947d7e852cb0bd520af3a31f034b
luisv7 <luisvillarreal_7@icloud.com>
2022-10-21 15:47:05 -0700

    new menu layout

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,70 +1,102 @@
-import { useEffect, useState } from "react";
-import { AppBar, Box, IconButton, Toolbar, Drawer, List, ListItem, ListItemButton, ListItemText, Link } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-
+import { useState } from "react";
+import {
+    AppBar,
+    Box,
+    IconButton,
+    Toolbar,
+    Drawer,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+    Link,
+    Container,
+    Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const MenuBar = () => {
-
-    const [navToggle, setNavToggle] = useState(false)
+    const [navToggle, setNavToggle] = useState(false);
 
     const handleNavClick = () => () => {
-        setNavToggle(prevState => !prevState)
+        setNavToggle((prevState) => !prevState);
     };
 
-    useEffect(() => {
-
-    })
-
     return (
-        <AppBar primary='main' sx={{ height: '60px', backgroundColor: "#fff"}}>
-            <Toolbar sx={{ display: "flex", justifyContent: "space-between"}}>
-                <>
-                    <IconButton 
-                        edge='start'
+        <AppBar primary="main" sx={{ height: "57px", backgroundColor: "#fff" }}>
+            <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Container
+                    maxWidth={false}
+                    sx={{ display: "flex", alignItems: "center" }}
+                >
+                    <IconButton
+                        edge="start"
                         aria-label="menu"
                         sx={{ mr: 2 }}
                         onClick={handleNavClick()}
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Link href="/" underline="none" sx={{ color: 'black'}}>
-                        <h3>Luis Villarreal</h3>
+                    <Link href="/" underline="none" sx={{ color: "black" }}>
+                        <Typography
+                            variant="subtitle1"
+                            component="h1"
+                            sx={{ fontWeight: "600" }}
+                        >
+                            Luis Villarreal
+                        </Typography>
                     </Link>
-                </>
-                <div className="social">
-                    <Link href="https://github.com/luisv7" sx={{color: "black"}}>
-                        <GitHubIcon sx={{margin: "0 0.5rem", fontSize: "32px"}}></GitHubIcon>
-                    </Link>
-                    <Link href="https://www.linkedin.com/in/luis-villarreal7/" sx={{color: "black"}}>
-                        <LinkedInIcon sx={{margin: "0 0.5rem", fontSize: "32px"}}></LinkedInIcon>
-                    </Link>
-                </div>
-            </Toolbar>
-            <div>
-                <Drawer
-                    anchor='left'
-                    open={navToggle}
-                    onClose={handleNavClick()}
+                </Container>
+                <Container
+                    maxWidth={false}
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "flex-end",
+                    }}
                 >
-                    <Box
-                        sx={{width: 200}}
+                    <Link
+                        href="https://github.com/luisv7"
+                        sx={{ color: "black" }}
                     >
-                        <List>
-                        {['Project 1', 'Project 2', 'Project 3', 'Resume'].map((text) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton href={"/" + text.replace(' ', '').toLowerCase()}>
+                        <GitHubIcon
+                            sx={{ margin: "0 0.5rem", fontSize: "32px" }}
+                        ></GitHubIcon>
+                    </Link>
+                    <Link
+                        href="https://www.linkedin.com/in/luis-villarreal7/"
+                        sx={{ color: "black" }}
+                    >
+                        <LinkedInIcon
+                            sx={{ margin: "0 0.5rem", fontSize: "32px" }}
+                        ></LinkedInIcon>
+                    </Link>
+                </Container>
+            </Toolbar>
+            <Drawer anchor="left" open={navToggle} onClose={handleNavClick()}>
+                <Box sx={{ width: 200 }}>
+                    <List>
+                        {["Project 1", "Project 2", "Project 3", "Resume"].map(
+                            (text) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton
+                                        href={
+                                            "/" +
+                                            text.replace(" ", "").toLowerCase()
+                                        }
+                                    >
                                         <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                        </List>
-                    </Box>
-                </Drawer>
-            </div>
+                                    </ListItemButton>
+                                </ListItem>
+                            )
+                        )}
+                    </List>
+                </Box>
+            </Drawer>
         </AppBar>
-    )
-}
+    );
+};
 
 export default MenuBar;
